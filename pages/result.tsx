8c25458fceb7ada816aa981e246dd637cfe9db22
TheRoyalMuffinMan <totalhockeyman@gmail.com>
2023-03-05 00:49:59 -0600

    merging from main

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,16 +1,17 @@
-import { chakra } from '@chakra-ui/react';
+import { chakra, Flex, Spinner } from '@chakra-ui/react';
 import Header from './components/Header';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Result() {
     const router = useRouter();
+    const [result, setResult] = useState<string | undefined>(undefined);
 
-    useEffect(() => {
-        if (!router.query || Object.keys(router.query).length === 0) {
-            router.push("/");
-        }
-    }, [router]);
+    // useEffect(() => {
+    //     if (!router.query || Object.keys(router.query).length === 0) {
+    //         router.push("/");
+    //     }
+    // }, [router]);
 
     async function analysis(params: object) {
         const response = await fetch("/api/analyzer", {
@@ -26,9 +27,32 @@ export default function Result() {
         console.log(result);
     }
 
-    useEffect(() => {
-        analysis(router.query);
-    });
+    // useEffect(() => {
+    //     analysis(router.query);
+    // });
+
+
+    if (result === undefined) {
+        return (
+            <chakra.div 
+                display="flex" 
+                flexDirection="column" 
+                minHeight="100vh"
+                bg="#0F172A"
+            >
+                <Header />
+                <Flex justify="center" align="center">
+                    <Spinner
+                        thickness='4px'
+                        speed='0.65s'
+                        emptyColor='gray.200'
+                        color='blue.500'
+                        size='xl'
+                    />
+                </Flex>
+            </chakra.div>
+        )
+    }
 
 
 
